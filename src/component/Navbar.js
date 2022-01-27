6abcf56d2a5fbe3d3fd339da95194491d9bd9d38
karanjot <karanjot_khatter@hotmail.co.uk>
2022-01-27 19:57:51 +0000

    tablet and mobile version of homepage completed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/shared/logo.svg'
 import { Link } from 'react-router-dom';
 import '../css/navbar.css';
+import { useLocation } from 'react-router-dom';
+
 
 const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => {
+        setClick(!click);
+    }
+  
+  const location = useLocation();
   return <div>
       <Link to="/" >
         <img src={logo} className='logo' alt='logo'></img>
       </Link>
       <hr className='line' />
       <div className="navbar">
-        <ul>
+      <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
-                <Link to="/" className='nav-links' >
-                  <strong>00</strong>
+                <Link to="/" className={location.pathname === '/' ? 'nav-links active' : 'nav-links'} >
+                  <strong className='nav-item_number'>00</strong>
                   <span>HOME</span>
                 </Link>
             </li>
             <li className="nav-item">
-                <Link to="/destination" className='nav-links' >
-                    <strong>01</strong>
+                <Link to="/destination" className={location.pathname === '/destination' ? 'nav-links active' : 'nav-links'} >
+                    <strong className='nav-item_number'>01</strong>
                     <span>DESTINATION</span>
                 </Link>
             </li>
             <li className="nav-item">
-                <Link to="/crew" className='nav-links'>
-                    <strong>02</strong>
+                <Link to="/crew" className={location.pathname === '/crew' ? 'nav-links active' : 'nav-links'}>
+                    <strong className='nav-item_number'>02</strong>
                     <span>CREW</span>
                 </Link>
             </li>
             <li className="nav-item">
-                <Link to="/technology" className='nav-links'>
-                    <strong>03</strong>
+                <Link to="/technology" className={location.pathname === '/technology' ? 'nav-links active' : 'nav-links'}>
+                    <strong className='nav-item_number'>03</strong>
                     <span>TECHNOLOGY</span>
                 </Link>
             </li>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../component/Navbar';
 import '../css/home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return <div className='homepage'>
@@ -10,6 +11,11 @@ const Home = () => {
         <h1>SPACE</h1>
         <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
       </div>
+      <Link to="/destination">
+        <div className='homepage_explore-circle'>
+            EXPLORE
+        </div>
+      </Link>
   </div>;
 };
 
